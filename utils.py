880d6276defa32d5713e24cefc855b5df2948040
iindyk <ihor.indyk@gmail.com>
2019-01-26 16:13:01 -0500

    S&P 500 companies names + prices fetcher added

--- utils.py
+++ utils.py
@@ -1,10 +1,3 @@
-import bs4 as bs
-import datetime as dt
-import os
-import pandas_datareader.data as web
-import pickle
-import requests
-
 
 def get_adj_returns(n):
     # todo
@@ -20,43 +13,3 @@ def cvar(alpha, prices):
     # todo
     return None
 
-
-def save_sp500_tickers():
-    resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-    soup = bs.BeautifulSoup(resp.text, 'lxml')
-    table = soup.find('table', {'class': 'wikitable sortable'})
-    tickers = []
-    for row in table.findAll('tr')[1:]:
-        ticker = row.findAll('td')[0].text
-        tickers.append(ticker)
-    with open("sp500tickers.pickle", "wb") as f:
-        pickle.dump(tickers, f)
-    return tickers
-
-
-# save_sp500_tickers()
-def get_data_from_yahoo(reload_sp500=False):
-    if reload_sp500:
-        tickers = save_sp500_tickers()
-    else:
-        with open("sp500tickers.pickle", "rb") as f:
-            tickers = pickle.load(f)
-    if not os.path.exists('stock_dfs'):
-        os.makedirs('stock_dfs')
-
-    start = dt.datetime(2010, 1, 1)
-    end = dt.datetime.now()
-    for ticker in tickers:
-        # just in case your connection breaks, we'd like to save our progress!
-        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
-            df = web.DataReader(ticker, 'morningstar', start, end)
-            df.reset_index(inplace=True)
-            df.set_index("Date", inplace=True)
-            df = df.drop("Symbol", axis=1)
-            df.to_csv('stock_dfs/{}.csv'.format(ticker))
-        else:
-            print('Already have {}'.format(ticker))
-
-
-if __name__ == "__main__":
-    get_data_from_yahoo()
