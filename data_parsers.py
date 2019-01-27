359731bfd41329e081e4dfc06cbb97cff925bfcc
iindyk <ihor.indyk@gmail.com>
2019-01-27 11:27:08 -0500

    S&P 500 companies names + prices fetcher added

--- data_parsers.py
+++ data_parsers.py
@@ -4,6 +4,7 @@ import os
 import pandas_datareader.data as web
 import pickle
 import requests
+import pandas as pd
 
 
 def save_sp500_tickers():
@@ -35,14 +36,37 @@ def get_data_from_yahoo(reload_sp500=False):
     for ticker in tickers:
         # just in case your connection breaks, we'd like to save our progress!
         if not os.path.exists('stock_dfs/{}.csv'.format(ticker['ticker'])):
-            df = web.DataReader(ticker['ticker'], 'morningstar', start, end)
+            df = web.DataReader(ticker['ticker'], 'yahoo', start, end)
             df.reset_index(inplace=True)
             df.set_index("Date", inplace=True)
-            df = df.drop("Symbol", axis=1)
             df.to_csv('stock_dfs/{}.csv'.format(ticker['ticker']))
         else:
             print('Already have {}'.format(ticker['ticker']))
 
 
+def compile_data():
+    with open("sp500tickers.pickle", "rb") as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+
+    for count, ticker in enumerate(tickers):
+        df = pd.read_csv('stock_dfs/{}.csv'.format(ticker['ticker']))
+        df.set_index('Date', inplace=True)
+
+        df.rename(columns={'Adj Close': ticker['ticker']}, inplace=True)
+        df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
+
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how='outer')
+
+        if count % 10 == 0:
+            print(count)
+    print(main_df.head())
+    main_df.to_csv('sp500_joined_closes.csv')
+
+
 if __name__ == "__main__":
-    get_data_from_yahoo()
+    compile_data()
